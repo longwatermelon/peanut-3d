c7b797fd9c9903b36534adc52138f58510cc51c3
longwatermelon <qianhansenb@gmail.com>
2021-03-11 16:17:49 -0700

    mouse captures position

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "include/peanut.h"
 #include <iostream>
+#include <Windows.h>
 
 
 int main(int argc, char** argv)
@@ -14,6 +15,8 @@ int main(int argc, char** argv)
 	std::vector<peanut::Object> cubes;
 	std::vector<peanut::Object> donuts;
 
+	
+
 	while (running)
 	{
 		while (SDL_PollEvent(&evt))
@@ -24,22 +27,32 @@ int main(int argc, char** argv)
 			case SDL_KEYDOWN:
 				switch (evt.key.keysym.sym)
 				{
-				case SDLK_SPACE: cam.y += 0.1f; break;
-				case SDLK_LSHIFT: cam.y -= 0.1f; break;
-				case SDLK_w: cam.z -= 0.1f; break;
-				case SDLK_s: cam.z += 0.1f; break;
-				case SDLK_a: cam.x += 0.1f; break;
-				case SDLK_d: cam.x -= 0.1f; break;
-				case SDLK_UP: cam.va -= 0.1f; break;
-				case SDLK_DOWN: cam.va += 0.1f; break;
-				case SDLK_RIGHT: cam.ha += 0.1f; break;
-				case SDLK_LEFT: cam.ha -= 0.1f; break;
 				case SDLK_0: cubes.clear(); break;
 				case SDLK_1: cubes.push_back(peanut::Object(-cam.x, -cam.y, -cam.z + 4, "meshfiles/cube.facet")); break;
 				case SDLK_2: donuts.push_back(peanut::Object(-cam.x, -cam.y, -cam.z + 4, "meshfiles/donut.facet")); break;
 				}
 			}
 		}
+
+		if (GetAsyncKeyState('W') < 0) cam.z -= 0.1f;
+		if (GetAsyncKeyState('S') < 0) cam.z += 0.1f;
+		if (GetAsyncKeyState('A') < 0) cam.x += 0.1f;
+		if (GetAsyncKeyState('D') < 0) cam.x -= 0.1f;
+		if (GetAsyncKeyState(VK_UP) < 0) cam.va -= 0.1f;
+		if (GetAsyncKeyState(VK_DOWN) < 0) cam.va += 0.1f;
+		if (GetAsyncKeyState(VK_RIGHT) < 0) cam.ha += 0.1f;
+		if (GetAsyncKeyState(VK_LEFT) < 0) cam.ha -= 0.1f;
+		if (GetAsyncKeyState(VK_SHIFT) < 0) cam.y -= 0.1f;
+		if (GetAsyncKeyState(VK_SPACE) < 0) cam.y += 0.1f;
+
+		if (GetAsyncKeyState(VK_ESCAPE)) SDL_SetRelativeMouseMode(SDL_FALSE);
+		if (GetAsyncKeyState(VK_TAB)) SDL_SetRelativeMouseMode(SDL_TRUE);
+
+		int x, y;
+		SDL_GetMouseState(&x, &y);
+
+		cam.ha = ((float)x / peanut::gfx->getw()) * (4 * 3.1415) - (2 * 3.1415);
+		cam.va = ((float)y / peanut::gfx->geth()) * (4 * 3.1415) - (2 * 3.1415);
 
 		peanut::clear_screen();
 
